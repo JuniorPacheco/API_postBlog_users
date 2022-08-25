ed06115bf8c212d032324c2c67fd82b6376272f3
Junior Pacheco <junior.pacheco.356@gmail.com>
2022-08-25 13:22:28 -0500

    my first commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+//* Dependencias
+const express = require("express");
+
+//*Archivos de rutas
+const userRouter = require("./users/users.router").router;
+const authRouter = require("./auth/auth.router").router;
+const postRouter = require("./posts/posts.router").router;
+
+//* Configuraciones iniciales
+const app = express();
+
+//? Esta configuracion es para habilitar el req.body
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "All ok!" });
+});
+
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/posts", postRouter)
+
+app.listen(8000, () => {
+  console.log("Server started at port 8000");
+});
